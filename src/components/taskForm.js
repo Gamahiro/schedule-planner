80bee851b6539397dca1d4b6303ad702ac5d5ff6
Johan <jpettersen50@gmail.com>
2023-04-02 20:21:41 +0200

    create task form submit event

--- src/components/taskForm.js
+++ src/components/taskForm.js
@@ -3,30 +3,48 @@ import { taskObj } from "../model/task";
 
 const TaskForm = (props) => {
 
-    function submitNewTask(e) {
-        e.preventDefault();
-        let newTask = new taskObj()
 
+    const [taskName, setTaskName] = React.useState('');
+    const [taskTime, setTaskTime] = React.useState('');
+    const [taskDescr, setTaskDescr] = React.useState('');
+
+    const handleTaskName = (e) => {
+        setTaskName(e.target.value)
+    }
+
+    const handleTaskTime = (e) => {
+        setTaskTime(e.target.value)
+    }
+
+    const handleTaskDescr = (e) => {
+        setTaskDescr(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let newTask = new taskObj(taskName, taskTime, props.day, taskDescr, false);
+        console.log(newTask)
     }
 
     return (
-        <form className="taskForm" onSubmit={this.submitNewTask}>
+        <form className="taskForm" onSubmit={handleSubmit}>
             <fieldset className="taskFieldset">
                 <legend>New Task</legend>
-                <div style={{alignSelf: "flex-end", color: "red", width: "1em", textAlign: "center"}}>x</div>
+                <div style={{ alignSelf: "flex-end", color: "red", width: "1em", textAlign: "center" }}>x</div>
                 <p>
                     <label htmlFor="taskName">Task: </label>
-                    <input type={"text"} name={"taskName"} id={"taskName"}></input>
+                    <input type={"text"} name={"taskName"} id={"taskName"} onChange={handleTaskName}></input>
                 </p>
 
                 <p>
                     <label htmlFor={"taskTime"}>Time: </label>
-                    <input type={"time"} min={"00:00"} max={"23:59"} id={"taskTime"}></input>
+                    <input type={"time"} min={"00:00"} max={"23:59"} id={"taskTime"} onChange={handleTaskTime}></input>
                 </p>
 
                 <p>
                     <label htmlFor={"taskDescr"}>Description: </label>
-                    <textarea id="taskDescr" type={""}></textarea>
+                    <textarea id="taskDescr" type={""} onChange={handleTaskDescr}></textarea>
                 </p>
 
                 <input className="button" type={"submit"}></input>
